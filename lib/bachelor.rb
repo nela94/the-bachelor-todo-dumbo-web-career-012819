ec82e32bac1e299e459c33674988b0e585d8fc4a
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-28 10:26:02 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -37,7 +37,15 @@ counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, array|
+     array.each do |hash|
+       hash.each do |k,v|
+         if v == hometown
+           return hash["occupation"]
+         end
+       end
+     end
+   end
 end
 
 def get_average_age_for_season(data, season)
